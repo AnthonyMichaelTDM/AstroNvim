1b3a314117cf4728428cd5e2c7c5fd68d57e83a2
AnthonyMichaelTDM <68485672+AnthonyMichaelTDM@users.noreply.github.com>
2024-05-28 19:50:04 -0700

    revert last commit

--- lua/plugins/rust-tools.lua
+++ lua/plugins/rust-tools.lua
@@ -19,10 +19,6 @@ return {
       "AstroNvim/astrolsp",
       ---@type AstroLSPOpts
       opts = {
-        -- enable servers that you already have installed without mason
-        servers = {
-          "rust_analyzer"
-        },
         setup_handlers = {
           -- add custom handler
           rust_analyzer = function(_, opts)
